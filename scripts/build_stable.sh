8ca9afbdc7549dde35694fd9c1ceb223469e2e8c
Siss6678 <86250182+Siss6678@users.noreply.github.com>
2022-04-19 16:08:10 +0300

    Update build_stable.sh

--- scripts/build_stable.sh
+++ scripts/build_stable.sh
@@ -120,6 +120,7 @@ pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
 export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y.%m.%d"))/g" zzz-default-settings
+sed -i "s/Shanghai/Taipei/g" zzz-default-settings
 popd
 
 # Fix libssh
