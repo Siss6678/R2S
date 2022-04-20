635b0f5d48d6fbd00e68863eef02be4d1a0418d5
Siss6678 <86250182+Siss6678@users.noreply.github.com>
2022-04-20 04:02:54 +0300

    Update build_stable.sh

--- scripts/build_stable.sh
+++ scripts/build_stable.sh
@@ -187,7 +187,13 @@ rm -rf banner
 wget https://raw.githubusercontent.com/thomaswcy/R2S/main/banner
 popd
 
-# Add CUPInfo
+# Replace lede hostapd with immortalwrt source
+pushd package/network/services
+rm -rf hostapd
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/network/services/hostapd
+popd
+
+# Add CPUInfo
 pushd package/lean/autocore/files/arm/sbin
 cp -f $GITHUB_WORKSPACE/scripts/cpuinfo cpuinfo
 popd
